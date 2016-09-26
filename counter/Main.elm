e9a6234ad89ce786ad7cbf26b0d186a1267a78a1
WebDevFromScratch <mybettingstuff@gmail.com>
2016-09-26 22:58:42 +0200

    format with elm-format

--- counter/Main.elm
+++ counter/Main.elm
@@ -4,17 +4,23 @@ import Html exposing (..)
 import Html.App as App
 import Html.Events exposing (onClick)
 
+
 type alias Model =
-    { counter: Int
-    , incrementClicked: Int
-    , decrementClicked: Int
+    { counter : Int
+    , incrementClicked : Int
+    , decrementClicked : Int
     }
 
-type Msg = Increment | Decrement
+
+type Msg
+    = Increment
+    | Decrement
+
 
 model : Model
 model =
     Model 0 0 0
+
 
 update : Msg -> Model -> Model
 update msg model =
@@ -22,23 +28,30 @@ update msg model =
         Increment ->
             { counter = model.counter + 1
             , incrementClicked = model.incrementClicked + 1
-            , decrementClicked = model.decrementClicked }
+            , decrementClicked = model.decrementClicked
+            }
+
         Decrement ->
             { counter = model.counter - 1
             , incrementClicked = model.incrementClicked
-            , decrementClicked = model.decrementClicked + 1 }
+            , decrementClicked = model.decrementClicked + 1
+            }
+
 
 view : Model -> Html Msg
 view model =
     div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , text (toString model.counter)
-    , button [ onClick Increment ] [ text "+" ]
-    , h3 [] [ text (toString model.incrementClicked) ]
-    , h3 [] [ text (toString model.decrementClicked) ] ]
+        [ button [ onClick Decrement ] [ text "-" ]
+        , text (toString model.counter)
+        , button [ onClick Increment ] [ text "+" ]
+        , h3 [] [ text (toString model.incrementClicked) ]
+        , h3 [] [ text (toString model.decrementClicked) ]
+        ]
+
 
 main =
     App.beginnerProgram
         { model = model
         , view = view
-        , update = update }
+        , update = update
+        }
